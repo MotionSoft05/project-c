9f5e6c38f7b5affcb57509e80e0590e4876b5e99
MotionSoft05 <kevinbbarrios05@gmail.com>
2024-12-19 16:43:12 -0300

    Productos agregados, slider productos modificado

--- src/components/common/ProductosSlider.tsx
+++ src/components/common/ProductosSlider.tsx
@@ -5,7 +5,7 @@ import { motion } from "framer-motion";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import { Pagination, Navigation } from "swiper/modules";
-
+import CarwashVenta from "../../../public/assets/images/CarWashVenta.jpg";
 const CategoryList = () => {
   const [selectedCategory, setSelectedCategory] = useState(categoriesData[0]);
 
@@ -27,11 +27,11 @@ const CategoryList = () => {
         className="relative w-full h-28 bg-gray-800 rounded-lg shadow-lg overflow-hidden mb-10"
       >
         <Image
-          src="/images/productos-header.jpg"
+          src={CarwashVenta}
           alt="Productos Header"
           layout="fill"
           objectFit="cover"
-          className="absolute inset-0 z-0"
+          className=""
         />
 
         <div className="absolute inset-0 bg-black bg-opacity-50 flex flex-col justify-center items-center text-center text-white p-4">
@@ -39,7 +39,7 @@ const CategoryList = () => {
             initial={{ opacity: 0, y: 20 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.8, delay: 0.2 }}
-            className="text-4xl md:text-5xl font-bold mb-2"
+            className="text-3xl md:text-5xl font-bold mb-2"
           >
             Nuestros Productos
           </motion.h2>
@@ -47,15 +47,18 @@ const CategoryList = () => {
             initial={{ opacity: 0, y: 20 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.8, delay: 0.4 }}
-            className="text-lg md:text-xl max-w-3xl"
+            className="text-md md:text-xl max-w-3xl"
           >
             Descubre nuestra amplia gama de productos diseñados para el cuidado,
             mantenimiento y mejora de tu vehículo.
           </motion.p>
         </div>
       </motion.div>
-
       {/* Slider de Categorías */}
+      {/* Texto introductorio para el slider de categorías */}
+      <h2 className="text-xl text-black md:text-4xl font-bold mb-2">
+        Categorías disponibles
+      </h2>
       <Swiper
         spaceBetween={20}
         slidesPerView={2}
@@ -74,6 +77,11 @@ const CategoryList = () => {
               initial="hidden"
               whileInView="visible"
               viewport={{ once: true }}
+              animate={
+                selectedCategory.id === category.id
+                  ? { scale: 5, y: -10 } // Más grande y elevado
+                  : { scale: 1, y: 0 }
+              }
               transition={{ duration: 0.5 }}
               variants={{
                 hidden: { opacity: 0, scale: 0.95 },
@@ -98,12 +106,13 @@ const CategoryList = () => {
                 </motion.div>
               )}
 
-              <div className="absolute inset-0 bg-gradient-to-t from-black to-transparent flex flex-col justify-end p-4 text-white">
+              <div className="absolute inset-0 bg-gradient-to-t from-black to-transparent flex flex-col justify-end p-4 text-white overflow-hidden">
                 <motion.h3
                   initial={{ opacity: 0, y: 30 }}
                   whileInView={{ opacity: 1, y: 0 }}
                   transition={{ duration: 0.8 }}
-                  className="text-lg font-semibold"
+                  className="text-lg font-semibold truncate"
+                  title={category.title} // Tooltip para texto truncado
                 >
                   {category.title}
                 </motion.h3>
@@ -112,7 +121,24 @@ const CategoryList = () => {
           </SwiperSlide>
         ))}
       </Swiper>
-
+      {/* Aca quiero que este la descipcion de la caterogioa que se selecciono */}
+      {selectedCategory && (
+        <motion.div
+          initial="hidden"
+          animate="visible"
+          transition={{ duration: 0.8 }}
+          variants={{
+            visible: { opacity: 1, y: 0 },
+            hidden: { opacity: 0, y: -50 },
+          }}
+          className="my-6 text-center"
+        >
+          <h2 className="text-2xl font-bold text-gray-800">
+            {selectedCategory.title}
+          </h2>
+          <p className="text-gray-600">{selectedCategory.description}</p>
+        </motion.div>
+      )}
       {/* Slider de Productos */}
       {selectedCategory && (
         <div>
@@ -124,11 +150,7 @@ const CategoryList = () => {
               visible: { opacity: 1, y: 0 },
               hidden: { opacity: 0, y: -50 },
             }}
-            className="mb-10"
           >
-            <h3 className="text-3xl font-bold mb-5">
-              {selectedCategory.title}
-            </h3>
             <Swiper
               spaceBetween={20}
               slidesPerView={2}
@@ -138,12 +160,11 @@ const CategoryList = () => {
               }}
               navigation={true}
               modules={[Pagination, Navigation]}
-              className="mb-10"
             >
               {selectedCategory.products.map((product) => (
                 <SwiperSlide key={product.id}>
                   <motion.div
-                    className="relative w-full h-64 bg-white rounded-lg shadow-md overflow-hidden"
+                    className="relative w-full h-64  rounded-lg shadow-md overflow-hidden mb-8"
                     initial="hidden"
                     whileInView="visible"
                     viewport={{ once: true }}
@@ -167,12 +188,21 @@ const CategoryList = () => {
                       <h4 className="text-lg font-semibold">{product.title}</h4>
                       <p className="text-xs">{product.description}</p>
                       <div className="mt-2 flex items-center justify-between">
-                        <span className="text-xl font-bold">
-                          ${product.discountPrice}
-                        </span>
-                        <span className="text-sm line-through text-gray-400">
-                          ${product.price}
-                        </span>
+                        {/* Condición para mostrar el precio de descuento */}
+                        {product.discountPrice ? (
+                          <>
+                            <span className="text-xl font-bold">
+                              ${product.discountPrice}
+                            </span>
+                            <span className="text-sm line-through text-gray-400">
+                              ${product.price}
+                            </span>
+                          </>
+                        ) : (
+                          <span className="text-xl font-bold">
+                            ${product.price}
+                          </span>
+                        )}
                       </div>
                     </div>
                   </motion.div>
